48166851291069c2c5e96177556adc7f7623f8f3
Alexandru Macocian <amacocian@yahoo.com>
2026-02-11 16:33:40 +0100

    Updates to dap

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -11,13 +11,17 @@ return {
   {
     "mfussenegger/nvim-dap",
     opts = function()
+      local dap = require("dap")
+
       -- Prevent .vscode/launch.json from polluting the dap picker
-      require("dap").providers.configs["dap.launch.json"] = nil
+      dap.providers.configs["dap.launch.json"] = nil
+
+      -- Ensure dotnet is in PATH for netcoredbg
+      vim.env.PATH = vim.env.PATH .. ":/usr/bin"
 
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "razor",
         callback = function()
-          local dap = require("dap")
           if dap.configurations.cs and #dap.configurations.cs > 0 then
             dap.configurations.razor = dap.configurations.cs
           end
