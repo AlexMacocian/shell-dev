91cf6850a1c2dde6a2f81641fff693ac446e5059
Alexandru Macocian <amacocian@yahoo.com>
2026-03-21 12:20:27 +0100

    Mermaid ascii preview

---
+++ .config/nvim/lua/plugins/ascii-mermaid.lua
@@ -0,0 +1,11 @@
+return {
+  "kais-radwan/ascii-mermaid",
+  ft = "markdown",
+  opts = {
+    display_mode = "hybrid",
+    use_ascii = false,
+    auto = true,
+    debounce_ms = 500,
+    hybrid_threshold = 15,
+  },
+}
